19c70951f55cb0fc1dafb20ccaaf3c93bc05175b
mthlucas <m.lucasedu@gmail.com>
2016-10-20 19:17:15 -0200

    Add files via upload

---
+++ Gui_Sinal.m
@@ -0,0 +1,286 @@
+function varargout = Gui_Sinal(varargin)
+% GUI_SINAL MATLAB code for Gui_Sinal.fig
+%      GUI_SINAL, by itself, creates a new GUI_SINAL or raises the existing
+%      singleton*.
+%
+%      H = GUI_SINAL returns the handle to a new GUI_SINAL or the handle to
+%      the existing singleton*.
+%
+%      GUI_SINAL('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in GUI_SINAL.M with the given input arguments.
+%
+%      GUI_SINAL('Property','Value',...) creates a new GUI_SINAL or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before Gui_Sinal_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to Gui_Sinal_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help Gui_Sinal
+
+% Last Modified by GUIDE v2.5 19-Oct-2016 22:51:38
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @Gui_Sinal_OpeningFcn, ...
+                   'gui_OutputFcn',  @Gui_Sinal_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before Gui_Sinal is made visible.
+function Gui_Sinal_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to Gui_Sinal (see VARARGIN)
+
+% Choose default command line output for Gui_Sinal
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes Gui_Sinal wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = Gui_Sinal_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+
+
+function num_Callback(hObject, eventdata, handles)
+% hObject    handle to num (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of num as text
+%        str2double(get(hObject,'String')) returns contents of num as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function num_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to num (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function tx_Callback(hObject, eventdata, handles)
+% hObject    handle to tx (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of tx as text
+%        str2double(get(hObject,'String')) returns contents of tx as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function tx_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to tx (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function den_Callback(hObject, eventdata, handles)
+% hObject    handle to den (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of den as text
+%        str2double(get(hObject,'String')) returns contents of den as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function den_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to den (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in S.
+function S_Callback(hObject, eventdata, handles)
+% hObject    handle to S (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in Z.
+function Z_Callback(hObject, eventdata, handles)
+% hObject    handle to Z (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in resp.
+function resp_Callback(hObject, eventdata, handles)
+% hObject    handle to resp (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+
+function txa_Callback(hObject, eventdata, handles)
+% hObject    handle to txa (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of txa as text
+%        str2double(get(hObject,'String')) returns contents of txa as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function txa_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to txa (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton14.
+
+
+
+% --- Executes on button press in pushbutton15.
+function pushbutton15_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton15 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+f=str2double(get(handles.freq,'String'));
+t=0:0.01:10;
+x=sin(2*pi*f*t);
+axes(handles.axes1);
+plot(t,x);
+
+
+function freq_Callback(hObject, eventdata, handles)
+% hObject    handle to freq (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of freq as text
+%        str2double(get(hObject,'String')) returns contents of freq as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function freq_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to freq (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton17.
+function pushbutton17_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton17 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+f=str2double(get(handles.freq,'String'));
+tx=str2double(get(handles.txa,'String'));
+t=0:tx:10;
+x=sin(2*pi*f*t);
+axes(handles.axes5);
+stairs(t,x);
+
+% --- Executes on button press in pushbutton18.
+function pushbutton18_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton18 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+f=str2double(get(handles.freq,'String'));
+tx=str2double(get(handles.txa,'String'));
+t=0:tx:10;
+x=sin(2*pi*f*t);
+axes(handles.axes4);
+stem(t,x);
+
+
+% --- Executes on button press in pushbutton22.
+function pushbutton22_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton22 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+num=[str2num(get(handles.num,'String'))];
+den=[str2num(get(handles.den,'String'))];
+H=tf(num,den)
+
+
+% --- Executes on button press in pushbutton23.
+function pushbutton23_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton23 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+num=[str2num(get(handles.num,'String'))];
+den=[str2num(get(handles.den,'String'))];
+H=tf(num,den)
+tx=str2num(get(handles.tx,'String'));
+h=c2d(H,tx,'matched')
+
+% --- Executes on button press in pushbutton24.
+function pushbutton24_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton24 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+num=[str2num(get(handles.num,'String'))];
+den=[str2num(get(handles.den,'String'))];
+H=tf(num,den)
+tx=str2num(get(handles.tx,'String'));
+h=c2d(H,tx,'matched')
+axes(handles.axes6);
+step(H,h);
